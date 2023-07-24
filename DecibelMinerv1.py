9a19d09295b987c07a99ff4a7bf13bd241f0e077
frynetworks <307862181+frynetworks@users.noreply.github.com>
2023-07-24 13:57:08 -0500

    Update DecibelMinerv1.py
    Hardcoded FTP information until we can get a more secure method

--- DecibelMinerv1.py
+++ DecibelMinerv1.py
@@ -4,8 +4,6 @@ import datetime
 import os
 import pysftp
 import uuid
-from cryptography.fernet import Fernet
-import json
 
 def get_decibel(data):
     fourier = np.fft.fft(data)
@@ -19,13 +17,13 @@ def write_to_log(db, current_file):
     with open(current_file, 'a') as f:
         f.write(f"{now.strftime('%H:%M:%S')} - {db}\n")
 
-def upload_to_sftp(current_file, config):
+def upload_to_sftp(current_file, host, username, password):
     now = datetime.datetime.now()
     local_filename = current_file
     remote_filename = f"/home/fryscrypto/indoor_decibel/FRYdecibels_{mac}_{now.strftime('%m%d%Y_%H%M%S')}.log"
     cnopts = pysftp.CnOpts()
     cnopts.hostkeys = None 
-    with pysftp.Connection(config['host'], username=config['username'], password=config['password'], cnopts=cnopts) as sftp:
+    with pysftp.Connection(host, username=username, password=password, cnopts=cnopts) as sftp:
         sftp.put(local_filename, remote_filename)
     os.remove(local_filename)  # removes local file after upload
 
@@ -33,14 +31,10 @@ p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
 mac = '-'.join(['{:02x}'.format((uuid.getnode() >> i) & 0xff) for i in range(0,8*6,8)][::-1])
 
-with open("key.key", "rb") as key_file:
-    key = key_file.read()
-
-with open("config.json.enc", "rb") as file:
-    encrypted_config = file.read()
-
-cipher = Fernet(key)
-config = json.loads(cipher.decrypt(encrypted_config))
+# FTP Credentials
+ftp_host = '207.244.74.204'
+ftp_username = 'fryscrypto'
+ftp_password = 'Wtf.7001'
 
 last_upload_hour = datetime.datetime.now().hour
 # Initialize the current_file variable
@@ -57,7 +51,7 @@ while True:
     
     # Upload the file one minute before the top of the hour
     if now.minute == 59 and now.second == 0:
-        upload_to_sftp(current_file, config)
+        upload_to_sftp(current_file, ftp_host, ftp_username, ftp_password)
 
     # Update the filename at the top of the hour
     if now.minute == 0 and now.second == 0 and now.hour != last_upload_hour:
